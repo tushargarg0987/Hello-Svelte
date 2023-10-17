a1f6ddd3cd4b5b16bf7e4caa01da4753e14a5ed1
Tushar <tushargarg0987@gmail.com>
2023-10-17 16:50:08 +0530

    Event handler

---
+++ src/Counter.svelte
@@ -0,0 +1,47 @@
+<script>
+	let count = 0;
+
+	function handleCounter(e){
+		console.log(e);
+		count++;
+	}
+</script>
+
+<main>
+	<button on:click={()=>{count = count + 1}}>Count {count}</button>
+	<button on:click={handleCounter}>Count {count}</button>
+</main>
+
+<style>
+	.underline{
+		text-decoration: underline;
+	}
+
+	.danger{
+		color: red;
+	}
+
+	.success{
+		color: olivedrab;
+	}
+
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import App from './App.svelte';
+import Counter from './Counter.svelte'
 
-const app = new App({
+// const app = new App({
+// 	target: document.body,
+// 	props: {
+// 		name: 'Tushar'
+// 	}
+// });
+
+const counter = new Counter({
 	target: document.body,
-	props: {
-		name: 'Tushar'
-	}
-});
+})
 
-export default app;
+// export default app;
+export default counter;
